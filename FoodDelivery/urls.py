89d37a661b56ff141349f82b2f209d541e8f57f5
Marek Nesvadba <briskcz@gmail.com>
2019-10-25 23:48:21 +0200

    added login and logout "screens"

--- FoodDelivery/urls.py
+++ FoodDelivery/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from django.conf.urls import url
+from django.conf.urls import include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+]
+
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
